5aebc614aa4dfd7e46117ba7b964eeaa4e4e9f13
Kieran Best <74361879+KieranBest@users.noreply.github.com>
2023-10-23 20:52:53 +0100

    Basic Movements Created
    Sprin &, Crouch
    Certain criteria must be met

--- Assets/Scripts/PlayerMovement1.cs
+++ Assets/Scripts/PlayerMovement1.cs
@@ -15,30 +15,61 @@ public class PlayerMovement1 : MonoBehaviour
     public LayerMask groundMask;
 
     Vector3 velocity;
+    Vector3 move;
     bool isGrounded;
+    bool isRunning = false;
+    bool isCrouched = false;
 
     void Update()
     {
+        // Movement
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if(isGrounded && velocity.y < 0) { 
-            velocity.y = -2f;
-        }
 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * horizontal + transform.forward * vertical;
-
         controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        // Jumping
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (isGrounded)
+        {
+            move = transform.right * horizontal + transform.forward * vertical;
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded && !isCrouched)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        velocity.y += gravity * Time.deltaTime;
+        // Sprinting
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded && vertical > 0 && !isCrouched)
+        {
+            speed = 9f;
+            isRunning = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) && isGrounded)
+        {
+            speed = 6f;
+            isRunning = false;
+        }
 
-        controller.Move(velocity * Time.deltaTime);
+        // Crouching
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrounded && !isRunning)
+        {
+            speed = 1.5f;
+            controller.height = 1.5f;
+            isCrouched = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            speed = 6f;
+            controller.height = 3f;
+            isCrouched = false;
+        }
     }
 }
