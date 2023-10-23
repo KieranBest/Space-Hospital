2c0e815d511fba57e705fcd7b782e4cf9c55c7ba
Kieran Best <74361879+KieranBest@users.noreply.github.com>
2023-10-23 19:35:36 +0100

    Basic Movements + scene

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float mouseSensitivity = 150f;
+
+    public Transform player;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= inputY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * inputX);
+    }
+}
